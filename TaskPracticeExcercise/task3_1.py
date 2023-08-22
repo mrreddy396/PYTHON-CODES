f6600103af6f4169876f84e4c3018a2666aeba18
mrreddy396 <peram.hemanth99488@gmail.com>
2023-08-22 22:39:32 +0530

    Updated

---
+++ TaskPracticeExcercise/task3_1.py
@@ -0,0 +1,33 @@
+# Integer
+age = 25
+
+# Float
+pi = 3.14159
+
+# String
+name = "Alice"
+
+# Boolean
+is_student = True
+
+# List
+fruits = ["apple", "banana", "orange"]
+
+# Tuple
+coordinates = (42.123, -71.456)
+
+# Dictionary
+person = {
+    "name": "Bob",
+    "age": 30,
+    "is_employee": False
+}
+
+# Printing the variables and their types
+print("Integer:", age, type(age))
+print("Float:", pi, type(pi))
+print("String:", name, type(name))
+print("Boolean:", is_student, type(is_student))
+print("List:", fruits, type(fruits))
+print("Tuple:", coordinates, type(coordinates))
+print("Dictionary:", person, type(person))
---
+++ TaskPracticeExcercise/task3_2.py
@@ -0,0 +1,18 @@
+numbers = [5, 12, 8, 25, 6, 14, 3, 18, 10, 22]
+total = 0
+# print the sum of even numbers
+for num in numbers:
+    if num % 2 == 0:
+        total = num + total
+print("Sum of even numbers:", total)
+
+# print the maximum value
+max_value = max(numbers)
+print("Maximum value:", max_value)
+
+# Create a new list of numbers greater than 10
+i = []
+for num in numbers:
+    if num > 10:
+        a = i.append(num)
+print("Numbers greater than 10:", i)
